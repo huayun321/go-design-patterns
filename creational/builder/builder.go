fafc708b20d944c82f213dfdb979455212105eb3
huayun321 <huayun321@gmail.com>
2021-01-01 11:15:40 +0800

    add builder

---
+++ creational/builder/builder.go
@@ -0,0 +1,60 @@
+package builder
+
+type BuildProcess interface {
+	SetWheels() BuildProcess
+	SetSeats() BuildProcess
+	SetStructure() BuildProcess
+	GetVehicle() VehicleProduct
+}
+
+type ManufacturingDirector struct{}
+
+func (f *ManufacturingDirector) Construct() {
+	//implement here
+}
+
+func (f *ManufacturingDirector) SetBuilder(b BuildProcess) {
+	//implement here
+}
+
+type VehicleProduct struct {
+	Wheels    int
+	Seats     int
+	Structure string
+}
+
+type CarBuilder struct{}
+
+func (c *CarBuilder) SetWheels() BuildProcess {
+	return nil
+}
+
+func (c *CarBuilder) SetSeats() BuildProcess {
+	return nil
+}
+
+func (c *CarBuilder) SetStructure() BuildProcess {
+	return nil
+}
+
+func (c *CarBuilder) GetVehicle() VehicleProduct {
+	return VehicleProduct{}
+}
+
+type BikeBuilder struct{}
+
+func (b *BikeBuilder) SetWheels() BuildProcess {
+	return nil
+}
+
+func (b *BikeBuilder) SetSeats() BuildProcess {
+	return nil
+}
+
+func (b *BikeBuilder) SetStructure() BuildProcess {
+	return nil
+}
+
+func (b *BikeBuilder) GetVehicle() VehicleProduct {
+	return VehicleProduct{}
+}
